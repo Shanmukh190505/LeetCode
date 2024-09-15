33012d35a032492504feb97e91d96a3f85ab6567
Shanmukh190505 <109965164+Shanmukh190505@users.noreply.github.com>
2024-09-15 19:35:06 +0530

    Time: 0 ms (100%), Space: 40.3 MB (81.54%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -3,12 +3,12 @@ class Solution {
         if(n==0){
             return 0;
         }
-        int sum=0;
-        for(int i=2;i<=n;i++){
-            sum=sum+i;
-        }
+        // int sum=0;
+        // for(int i=2;i<=n;i++){
+        //     sum=sum+i;
+        // }
         int res=0;
-        for(int i=5;i<=sum;i=i*5){
+        for(int i=5;i<=n;i=i*5){
             res=res+n/i;
         }
         return res;
